b77a328e5ab58e79d441cc872966a5c56bcedc19
Shivani90 <jyotishukla05274@gmail.com>
2024-11-05 23:43:01 +0530

    chng

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -24,7 +24,7 @@ const Recipes = () => {
             {pathname === "/recipes" && (
                 <Link
                     to="/create-recipe"
-                    className="cursor-pointer rounded-md absolute top-[15%] py-2 px-5 left-[10%]  bg-green-200 gap-x-3 flex items-center"
+                    className="cursor-pointer rounded-md absolute top-[15%] py-2 px-5 left-[10%]  bg-gray-200 gap-x-3 flex items-center"
                 >
                     <i className="text-3xl text-gray-600 ri-add-box-line"></i>
                     Create Recipe
