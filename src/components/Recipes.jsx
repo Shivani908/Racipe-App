51f22cbc9a989ca4919c96f0dc68c6fe07f53064
Shivani90 <jyotishukla05274@gmail.com>
2024-11-05 23:42:12 +0530

    chng

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -16,7 +16,7 @@ const Recipes = () => {
                 {recipes.length > 0 ? (
                     recipes.map((r) => <Card key={r.id} recipe={r} />)
                 ) : (
-                    <h1 className="w-full text-green-600 text-3xl font-extrabold text-center mt-10">
+                    <h1 className="w-full text-gray-600 text-3xl font-extrabold text-center mt-10">
                         No Recipe Found
                     </h1>
                 )}
@@ -26,7 +26,7 @@ const Recipes = () => {
                     to="/create-recipe"
                     className="cursor-pointer rounded-md absolute top-[15%] py-2 px-5 left-[10%]  bg-green-200 gap-x-3 flex items-center"
                 >
-                    <i className="text-3xl text-green-600 ri-add-box-line"></i>
+                    <i className="text-3xl text-gray-600 ri-add-box-line"></i>
                     Create Recipe
                 </Link>
             )}
