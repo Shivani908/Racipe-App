81a8cbc9ff0363826281ba3b506832031ff3395d
Shivani90 <jyotishukla05274@gmail.com>
2024-11-07 14:22:51 +0530

    change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <h1 className="text-7xl mt-5 font-extrabold text-gray-600 mb-[5%]">
                 DO YOU WANT TO REACH US ?
             </h1>
-            <button className="rounded-md text-md bg-green-600 text-white py-2 px-5 hover:bg-green-700 duration-200">
+            <button className="rounded-md text-md bg-gray-600 text-white py-2 px-5 hover:bg-gray-700 duration-200">
                 REACH KRLO FRAANDS &nbsp; &#8594;
             </button>
         </div>
